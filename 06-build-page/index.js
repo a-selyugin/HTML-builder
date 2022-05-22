40ab1f0ebcc2dcc6c7394ecd10b37f98bbb4a9f2
a-selyugin <selyuginai@gmail.com>
2022-05-22 17:33:55 +0300

    feat: complete 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const projDist = path.join(__dirname, 'project-dist');
 const stylesFolder = path.join(__dirname, 'styles');
 const assetsFolder = path.join(__dirname, 'assets');
+const componentsFolder = path.join(__dirname, 'components');
+const templateHtml = path.join(__dirname, 'template.html');
+const indexHtml = path.join(projDist, 'index.html');
 
 //
 // Создаем папку, и если она уже есть и не пустая, то удаляем все содержимое, 
@@ -17,7 +20,74 @@ function main() {
     }).catch(err => {
       console.log(err);
     }).then(() => buildCss())
-    .then(() => recursiveCopy(assetsFolder, 'assets', projDist));
+    .then(() => recursiveCopy(assetsFolder, 'assets', projDist))
+    .then(() => createCompArray(componentsFolder));
+}
+
+//
+// Читаем папку components и добавляем все содержимое HTML в массив объектов
+//
+
+// Сперва ищем все файлы компонентов в source, создаем массив объектов с данными о компонентах
+function createCompArray (source) {
+  fs.readdir(source,
+    { withFileTypes: true },
+    (err, files) => {
+      if (err) console.log(err);
+      else {
+        const compArray = [];
+        files.forEach(file => {
+          if (file.isFile()) {
+            const fileExt = path.extname(file.name);
+            const fileName = file.name.substring(0, file.name.lastIndexOf('.'));
+            const filePath = path.join(source, file.name);
+            if (fileExt === '.html'){
+              fs.readFile(filePath, 
+                (err, data) => {
+                  if (err) console.log(err);
+                  else {
+                    const compInstance = {
+                      name: fileName,
+                      content: data.toString(),
+                    };
+                    compArray.push(compInstance);
+                    processTemplateHTML(compArray);
+                  }
+                });
+            }
+          }
+        });
+      }
+    });
+}
+
+// Имея массив компонентов, читаем template.html, ищем все упоминания компонентов и вставляем их вместо шаблонных тегов
+// и пишем все в файл index.html
+
+function processTemplateHTML(arrayOfComponents){
+  const input = fs.createReadStream(templateHtml, 'utf-8');
+  const output = fs.createWriteStream(indexHtml);
+
+  input.on('data', chunk => {
+    let string = chunk.toString();
+    let componentFound;
+    do {
+      componentFound = false;
+      const startOfSubstring = string.indexOf('{{');
+      if (startOfSubstring !== -1) {
+        const endOfSubstring = string.indexOf('}}', startOfSubstring);
+        const componentName = string.substring(startOfSubstring + 2, endOfSubstring).trim();
+        const substr = string.substring(startOfSubstring, endOfSubstring + 2);
+        for (let i = 0; i < arrayOfComponents.length; i += 1) {
+          if (componentName === arrayOfComponents[i].name){
+            string = string.replace(substr, arrayOfComponents[i].content);
+          }
+        }
+        componentFound = true;
+      }
+    } while (componentFound);
+    output.write(string);
+  });
 }
 
 //
@@ -61,9 +131,8 @@ function bundleCss(cssArr, targetFolder) {
 }
 
 //
-// Рекурсивно копирует папку assets
+// Рекурсивно копируем папку assets
 //
-
 
 function recursiveCopy(source, sourceFolderName, target) {
   const targetPath = path.join(target, sourceFolderName);
@@ -103,5 +172,5 @@ function copyIfFiles(source, target) {
     }).catch(err => console.log(err));
 }
 
-
+// тело функции: удаляем существующую папку poject-dist и создаем запускаем main()
 fs.promises.rm(projDist, { recursive: true, force: true }, () => {console.log(`${projDist} cleared!`);}).then(() => main());
